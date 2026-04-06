1357910a2f4c728022b3a64fea7d74241612ef0d
Hodaahmed18 <royalpain687@gmail.com>
2026-04-06 15:05:39 +0000

    System Health Check

---
+++ system-health.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Script for monitoring RHEL systems
+
+
+echo "==== System Health Check ===="
+echo "Date: $(date)"
+echo ""
+
+echo "--- Disk Usage ---"
+df -h 
+echo ""
+
+
+echo " --- Memory ---"
+free -h 
+echo ""
+
+echo " --- Top 5 CPU Processes ---" 
+ps aux --sort=-%cpu | head -6
+echo ""
+
+echo " --- Uptime --- "
+uptime 
+echo ""
